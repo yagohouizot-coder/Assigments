c91e6ede7ab314a6a7c9ad32c3aa70198b48a415
yagohl <yagohouizot@gmail.com>
2025-10-30 18:25:31 +0100

    1st commit working code

--- a1/src/ecs.hpp
+++ a1/src/ecs.hpp
@@ -2,6 +2,8 @@
 
 #include <cstdlib>
 #include <cassert>
+#include <unordered_map>
+#include <vector>
 
 namespace ecs {
 using Entity = size_t;
@@ -16,24 +18,53 @@ public:
 	/// @brief Checks if the given entity is included in the container.
 	/// @param entity The entity to check.
 	/// @return If `entity` is included in the container.
-	bool has(const Entity entity) {}
+	bool has(const Entity entity) {
+		return sparse.find(entity) != sparse.end();
+	}
 
 	/// @brief Retrieve the component associated with the given entity.
 	/// @param entity The entity whose component will be retrieved.
 	/// @return A reference to the component associated to `entity`.
-	T& get(const Entity entity) {}
+	T& get(const Entity entity) {
+		assert(has(entity) && "Entity was not found in the container"); //assert used to stop execution if false
+		return dComponents[sparse[entity]];
+	}
 
 	/// @brief Remove the component associated with the given entity.
 	/// @param entity The entity whose component will be removed.
-	void remove(const Entity entity) {}
+	void remove(const Entity entity) {
+		if(!has(entity)) return;
+
+		size_t index = sparse[entity];
+		Entity lastE = dEntities.back();
+		T lastC = dComponents.back();
+
+		dEntities[index] = lastE;
+		dComponents[index] = lastC;
+		sparse[lastE] = index;
+
+		dEntities.pop_back();
+		dComponents.pop_back();
+		sparse.erase(entity);
+	}
 
 	/// @brief Add an association between the given entity and component.
 	/// @param entity The entity to add the association for.
 	/// @param component The component to add.
 	/// @return A reference to the newly added component.
-	T& emplace(const Entity entity, const T& component) {}
+	T& emplace(const Entity entity, const T& component) {
+		if(has(entity)) return get(entity);
+
+		sparse[entity] = dComponents.size();
+		dEntities.push_back(entity);
+		dComponents.push_back(component);
+		return dComponents.back();
+	}
 private:
 	// TODO: Add member variables such as the contained components here.
+	std::unordered_map<Entity,size_t> sparse;
+	std::vector<Entity> dEntities;
+	std::vector<T> dComponents;
 };
 
-} // namespace ecs
+} 
--- a1/src/main.cpp
+++ a1/src/main.cpp
@@ -1,23 +1,61 @@
 #include <cstdlib>
+#include <iostream>
 #include <ecs.hpp>
+
+struct Swim {};
+struct Fly {};
+struct Walk {};
+
+
 
 class Registry {
 public:
 	Registry() = default;
-	ecs::Entity createEntity() {}
+	ecs::Entity createEntity() {
+		return nextEntity++;
+	}
 
 	// TODO: Add public members of the registry here.
 
 private:
 	// TODO: Add non-public members of the registry here.
+	ecs::Entity nextEntity = 0;
 };
 
 int main(void) {
 	Registry registry;
 
 	// TODO: Create and modify entities as described in the task.
-	//       Write some debugging info about the entities to the terminals after each chnage.
+	//       Write some debugging info about the entities to the terminals after each change.
+
+	ecs::Container<Swim> swim;
+	ecs::Container<Fly> fly;
+	ecs::Container<Walk> walk;
+
+	ecs::Entity salmon = registry.createEntity();
+	ecs::Entity chicken = registry.createEntity();
+
+	swim.emplace(salmon,Swim{});
+
+	swim.emplace(chicken,Swim{});
+	fly.emplace(chicken,Fly{});
+	walk.emplace(chicken,Walk{});
+
+	std::cout << "Salmon able to swim and chicken to swim,fly and walk\n";
+
+	fly.emplace(salmon, Fly{});
+	fly.remove(chicken);
+
+	std::cout << "Salmon is now able to fly, not the chicken \n";
+
+	std::cout << "Salmon can swim: " << swim.has(salmon) << "\n";
+	std::cout << "Salmon can fly : " << fly.has(salmon)  << "\n";
+	std::cout << "Chicken can fly: " << fly.has(chicken) << "\n";
+
 
 	// TODO: Are you using an "Array of Structs" or "Struct of Arrays"? Why is this the case?
+
+	std::cout << " The component storage model used is Struct of arrays\n";
+	std::cout << "Every type of component is stored in its own array\n";
 	return EXIT_SUCCESS;
 }
